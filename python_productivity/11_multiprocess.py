a82f3d53fb6935b233d7ed7dccd35b74a9554e33
MiracleWong <cfwr1991@126.com>
2021-04-01 14:43:04 +0800

    11 multiprocessing 并行计算

--- python_productivity/11_multiprocess.py
+++ python_productivity/11_multiprocess.py
@@ -1,17 +1,40 @@
 from multiprocessing import Pool,Queue
 import psutil
 import os
+import time
 
 # 逻辑cpu的个数
 count = psutil.cpu_count()
 
-# 用于存储进程值
+# 定义一个队列用于存储进程id
 queue = Queue()
 
 def f(x):
+    queue.put(os.getpid())
     return x*x
 
 if __name__ == '__main__':
+
+    # 并行计算时间统计
     with Pool(count*2) as p:
+        time1 = time.time()
         res = p.map(f, range(1, 101))
         print(f'计算平方的结果是:{res}')
+        time2 = time.time()
+
+    print(str(time2 - time1))
+    pids = set()
+    while not queue.empty():
+        pids.add(queue.get())
+
+    print(f'用到的进程id是：{pids}')
+
+    # 串行计算时间统计
+    list1 = []
+
+    time1 = time.time()
+    for i in range(1, 10001):
+        list1.append(f(i))
+    time2 = time.time()
+
+    print(str(time2 - time1))
